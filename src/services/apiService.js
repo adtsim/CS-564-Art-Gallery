46cf15b3acfd815eb8678d2e2580a2e1b33b35cb
Aditi Simha <adisimha@pdx.edu>
2024-06-07 19:42:22 -0700

    Correcting the API key variable name

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -3,7 +3,7 @@
 // encapsulating the logic needed to fetch data from the Rijksmuseum API.
 
 import axios from "axios";
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.VITE_APP_APOD_API_KEY;
 
 const API_URL = "https://www.rijksmuseum.nl/api/en/collection";
 
